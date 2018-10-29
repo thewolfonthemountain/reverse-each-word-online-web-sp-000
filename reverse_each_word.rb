0250409bbdb65ae809dd1e6be65ca2fb3dcfb0d5
Sean Langton <slangton1@gmail.com>
2018-10-29 02:53:56 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,6 @@
 def reverse_each_word(sentence)
+  string = ""
   array = sentence.split(" ")
-  array.collect do |word| word.reverse
+  array.collect do |word| word.reverse 
   end
-  array.join(" ")
 end
